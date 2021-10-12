3543562120e347565e86a5e3e74f841966f3d79c
acmenes <acmenescompositions@gmail.com>
2021-10-12 12:09:10 -0400

    working on backend routes for the api

--- api.js
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
--- backend/routes/chords.js
+++ backend/routes/chords.js
@@ -8,6 +8,13 @@ router.get("/", async function(req, res, next){
     return res.json({"chords": "chords"})
 });
 
+/** this needs to be set up according to how the chords
+are stored in the db */
+
+router.get("/:chordname", async function(req, res, next){
+    return res.json({"chord": "chord"})
+});
+
 router.get("/sample", async function(req, res, next){
     return res.json({
         key: 'C',
