d0df0c5f937f5b4d3eebeb483c04e969ecd31443
acmenes <acmenescompositions@gmail.com>
2021-10-20 14:02:27 -0400

    added a few basic chords to the db

--- backend/chords-schema.sql
+++ backend/chords-schema.sql
@@ -1,19 +1,9 @@
 CREATE TABLE chords (
     id SERIAL PRIMARY KEY,
-    key TEXT NOT NULL,
-    quality TEXT NOT NULL,
-    extensions TEXT,
-    inv INTEGER,
-    E_fret TEXT,
-    A_fret TEXT,
-    D_fret TEXT,
-    G_fret TEXT,
-    B_fret TEXT,
-    High_E_fret TEXT,
-    E_finger TEXT,
-    A_finger TEXT,
-    D_finger TEXT,
-    G_finger TEXT,
-    B_finger TEXT,
-    High_E_finger TEXT
+    roots TEXT NOT NULL,
+    qualities TEXT NOT NULL,
+    barres TEXT,
+    capo BOOLEAN,
+    frets TEXT,
+    fingers TEXT
 );
--- backend/chords-seed.sql
+++ backend/chords-seed.sql
@@ -1,18 +1,29 @@
-INSERT INTO chords (key, 
-                    quality, 
-                    extensions, 
-                    inv, 
-                    E_fret,
-                    A_fret,
-                    D_fret,
-                    G_fret,
-                    B_fret,
-                    High_E_fret,
-                    E_finger,
-                    A_finger,
-                    D_finger,
-                    G_finger,
-                    B_finger,
-                    High_E_finger)
-        VALUES ('C', 'Major', 'None', '0', 'x', '3', '2', '0', '1', '0', '0', '3', '2', '0', '1', '0'),
-                ('G', 'Major', 'None', '0', '3', '2', '0', '0', '0', '3', '3', '2', '0', '0', '0', '4');
+-- INSERT INTO chords (key, 
+--                     quality, 
+--                     extensions, 
+--                     inv, 
+--                     E_fret,
+--                     A_fret,
+--                     D_fret,
+--                     G_fret,
+--                     B_fret,
+--                     High_E_fret,
+--                     E_finger,
+--                     A_finger,
+--                     D_finger,
+--                     G_finger,
+--                     B_finger,
+--                     High_E_finger)
+--         VALUES ('C', 'Major', 'None', '0', 'x', '3', '2', '0', '1', '0', '0', '3', '2', '0', '1', '0'),
+--                 ('G', 'Major', 'None', '0', '3', '2', '0', '0', '0', '3', '3', '2', '0', '0', '0', '4');
+
+INSERT INTO CHORDS(roots, qualities, barres, capo, frets, fingers) 
+        VALUES  ('G', 'Major', 'None', 'False', '3, 2, 0, 0, 0, 3', '3, 2, 0, 0, 0, 4'),
+                ('A', 'Major', 'None', 'False', '0, 0, 2, 2, 2, 0', '0, 0, 2, 3, 4, 0'),
+                ('C', 'Major', 'None', 'False', '0, 3, 2, 0, 1, 0', '0, 3, 2, 0, 1, 0'),
+                ('D', 'Major', 'None', 'False', '0, 0, 0, 2, 3, 2', '0, 0, 0, 1, 3, 2'),
+                ('E', 'Major', 'None', 'False', '0, 2, 2, 1, 0, 0', '0, 2, 3, 1, 0, 0'),
+                ('A', 'Minor', 'None', 'False', '0, 0, 2, 2, 1, 0', '0, 0, 2, 3, 1, 0'),
+                ('D', 'Minor', 'None', 'False', '0, 0, 0, 2, 3, 1', '0, 0, 0, 2, 4, 1'),
+                ('E', 'Minor', 'None', 'False', '0, 2, 2, 0, 0, 0', '0, 2, 3, 0, 0, 0');
+               
